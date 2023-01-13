b4aae4b7871eea4dc84ae9d598a37966fe1ec98b
Alper Karaca <96011403+thealper2@users.noreply.github.com>
2023-01-13 22:27:56 +0300

    Delete aritmetik1 directory

--- aritmetik1/target/debug/aritmetik1.d
+++
@@ -1,1 +0,0 @@
-/Users/alperkaraca/Desktop/Rust/aritmetik1/target/debug/aritmetik1: /Users/alperkaraca/Desktop/Rust/aritmetik1/src/main.rs
--- aritmetik1/target/debug/deps/aritmetik1-2d111f81e4fd9e6b.d
+++
@@ -1,5 +0,0 @@
-/Users/alperkaraca/Desktop/Rust/aritmetik1/target/debug/deps/aritmetik1-2d111f81e4fd9e6b: src/main.rs
-
-/Users/alperkaraca/Desktop/Rust/aritmetik1/target/debug/deps/aritmetik1-2d111f81e4fd9e6b.d: src/main.rs
-
-src/main.rs:
